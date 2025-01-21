81305314d3fbe4aacf044ddb6c59c0ddd3628055
Rupesh Virani <rupeshvirani01@gmail.com>
2025-01-21 20:23:57 +0530

    add cors.

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
